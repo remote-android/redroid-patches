5536686d25b7db6e425dc5acc0fe8b381f6a9cd9
Andy Lin <linhaomails@gmail.com>
2024-02-06 03:29:43 -0600

    apply-patch.sh: deduplicate found leaf dirs

--- apply-patch.sh
+++ apply-patch.sh
@@ -25,15 +25,11 @@ if [ ! -d $patch_dir ];then
     exit 1
 fi
 
-echo "Begin to apply patches from $patch_dir"
 cd $patch_dir
-pwd
-for p in `find *`
+leaf_dirs=$(find . -type d -exec sh -c '(ls -p "{}" | grep / > /dev/null) || echo "{}"' \;)
+for p in $leaf_dirs
 do
-    # if this is a patch file that ends with .patch
-    if [[ $p == *.patch ]]; then
-        project_dir=$(dirname $p)
-        echo "process project: $project_dir"
-        git -C $src/$project_dir apply --reject $patch_dir/$p || echo "*****[ERROR]***** apply failed: $p"
-    fi
+    echo
+    echo "process project: $p"
+    git -C $src/$p am --reject $patch_dir/$p/* || echo "*****[ERROR]***** apply failed: $p"
 done
